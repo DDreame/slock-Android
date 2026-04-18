abec65b76fcf9a83ba4c3b9b3fa64550f8dd59ea
DDreame <37373516+DDreame@users.noreply.github.com>
2026-04-18 08:23:28 +0000

    Fix #81 AgentListScreen import

--- project/SlockApp/app/src/main/java/com/slock/app/ui/agent/AgentListScreen.kt
+++ project/SlockApp/app/src/main/java/com/slock/app/ui/agent/AgentListScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
